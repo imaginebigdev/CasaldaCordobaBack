fd2d2a8c62af3f50a8942cda472254d7d32e48ee
Santirbe98 <santirbe@gmail.com>
2023-09-12 12:56:19 -0300

    DEPLOY #1

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,68 +3,80 @@ import { Dialect, Sequelize } from "sequelize";
 
 dotenv.config();
 
-interface DatabaseConfig {
-  development: {
-    dialect: string;
-    host: string;
-    database: string;
-    username: string;
-    password: string;
-  };
-  test: {
-    dialect: string;
-    host: string;
-    database: string;
-    username: string;
-    password: string;
-  };
-  production: {
-    dialect: string;
-    host: string;
-    database: string;
-    username: string;
-    password: string;
-  };
+// interface DatabaseConfig {
+//   development: {
+//     dialect: string;
+//     host: string;
+//     database: string;
+//     username: string;
+//     password: string;
+//   };
+//   test: {
+//     dialect: string;
+//     host: string;
+//     database: string;
+//     username: string;
+//     password: string;
+//   };
+//   production: {
+//     dialect: string;
+//     host: string;
+//     database: string;
+//     username: string;
+//     password: string;
+//   };
+// }
+
+// const env = process.env.NODE_ENV || "development";
+
+// const config: DatabaseConfig = {
+//   development: {
+//     dialect: "postgres",
+//     host: "localhost",
+//     database: process.env.DB_NAME || "nombre_de_la_db_dev",
+//     username: process.env.DB_USERNAME || "tu_usuario_dev",
+//     password: process.env.DB_PASSWORD || "tu_contraseña_dev",
+//   },
+//   test: {
+//     dialect: "postgres",
+//     host: "localhost",
+//     database: process.env.DB_NAME || "nombre_de_la_db_test",
+//     username: process.env.DB_USERNAME || "tu_usuario_test",
+//     password: process.env.DB_PASSWORD || "tu_contraseña_test",
+//   },
+//   production: {
+//     dialect: "postgres",
+//     host: "localhost",
+//     database: process.env.DB_NAME || "nombre_de_la_db_prod",
+//     username: process.env.DB_USERNAME || "tu_usuario_prod",
+//     password: process.env.DB_PASSWORD || "tu_contraseña_prod",
+//   },
+// };
+
+// const dbConfig = config[env as keyof DatabaseConfig];
+
+// if (!dbConfig) {
+//   throw new Error(`Invalid environment: ${env}`);
+// }
+
+// export const sequelize = new Sequelize(
+//   dbConfig.database,
+//   dbConfig.username,
+//   dbConfig.password,
+//   {
+//     dialect: dbConfig.dialect as Dialect,
+//     host: dbConfig.host,
+//   }
+// );
+
+//DEPLOY
+
+const dbUrl = process.env.DB_URL;
+
+if (!dbUrl) {
+  throw new Error("DB_URL environment variable is missing.");
 }
 
-const env = process.env.NODE_ENV || "development";
-
-const config: DatabaseConfig = {
-  development: {
-    dialect: "postgres",
-    host: "localhost",
-    database: process.env.DB_NAME || "nombre_de_la_db_dev",
-    username: process.env.DB_USERNAME || "tu_usuario_dev",
-    password: process.env.DB_PASSWORD || "tu_contraseña_dev",
-  },
-  test: {
-    dialect: "postgres",
-    host: "localhost",
-    database: process.env.DB_NAME || "nombre_de_la_db_test",
-    username: process.env.DB_USERNAME || "tu_usuario_test",
-    password: process.env.DB_PASSWORD || "tu_contraseña_test",
-  },
-  production: {
-    dialect: "postgres",
-    host: "localhost",
-    database: process.env.DB_NAME || "nombre_de_la_db_prod",
-    username: process.env.DB_USERNAME || "tu_usuario_prod",
-    password: process.env.DB_PASSWORD || "tu_contraseña_prod",
-  },
-};
-
-const dbConfig = config[env as keyof DatabaseConfig];
-
-if (!dbConfig) {
-  throw new Error(`Invalid environment: ${env}`);
-}
-
-export const sequelize = new Sequelize(
-  dbConfig.database,
-  dbConfig.username,
-  dbConfig.password,
-  {
-    dialect: dbConfig.dialect as Dialect,
-    host: dbConfig.host,
-  }
-);
+export const sequelize = new Sequelize(dbUrl, {
+  dialect: "postgres" as Dialect,
+});
